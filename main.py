6120a0f15313e8cf13ff32aa9cb4c964d5995c5f
Елена Баулина <elenabaulina@MacBook-Air-Aleksej-2.local>
2020-12-02 22:16:53 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+numbers = input("Введите числа : ")
+sum_1 = sum(map(int, numbers.split(' ')))
+print("Сумма введенных чисел = ", sum_1)
+numberss = input("Введите числа : ")
+def summ_numbers(numberss, sum_1):
+    summ = sum(map(int, numberss.split(' ')))
+    summ_all = summ + sum_1
+    print("Сумма всех введнных чисел равна = ", summ_all)
+
+summ_numbers(numberss, sum_1)
+
+
+
+
+
+
+
